93b47df6a3a086a1c0dc565122183c48da2f70ac
Jacques <jacquesaronius@gmail.com>
2018-05-08 15:51:41 -0500

    Started working on tempo function

--- Project3.c
+++ Project3.c
@@ -401,4 +401,25 @@ void transmit(char * st)
 		x++;	
 	}
 }
+
+void change_tempo(void)
+{
+	if (tempo == 125)
+	{
+		tempo = tempo / 2;
+		transmit("Tempo is at 120 bpm");
+	}
+	
+	else if (tempo == 62)
+	{
+		tempo = tempo / 2;
+		transmit("Tempo is at 240 bpm");
+	}
+	
+	else 
+	{
+		tempo = 125;
+		transmit("Tempo is reset to 60 bpm");
+	}
+}
 	
